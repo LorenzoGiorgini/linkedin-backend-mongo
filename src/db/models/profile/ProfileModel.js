21817d904de81ded50560634ee2bc7ad3eb22e7d
Zukhriddin <zuxr.akhmedov@gmail.com>
2021-11-15 11:32:31 +0000

    updates

--- src/db/models/profile/ProfileModel.js
+++ src/db/models/profile/ProfileModel.js
@@ -4,13 +4,14 @@ const { Schema, model} = mongoose
 const ProfileSchema = new Schema({
     name:{type:String,required:true},
     surname:{type:String,required:true},
-   email:{type:String,required:true},
+    email:{type:String,required:true},
     bio:{type:String,required:true},
     title:{type:String,required:true},
     area:{type:String,required:true},
     image:{type:String,required:true},
-    username:{type:String,required:true}
-
+    username:{type:String,required:true},
+    experiences:[
+        {type: Schema.Types.ObjectId, ref: "Experience"}]
 },
 {
     timestamps:true
